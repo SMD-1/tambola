e9ecbfb21cb2601024ac41d41cb50d326d4196a0
Danish <skdanish1999@gmail.com>
2021-05-23 02:28:28 +0530

    fix font

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-const DaysEl = document.getElementById('days');
-const HoursEl = document.getElementById('hours');
-const MinutesEl = document.getElementById('minutes');
-const SecondsEl = document.getElementById('seconds');
+const DaysEl = document.getElementById("days");
+const HoursEl = document.getElementById("hours");
+const MinutesEl = document.getElementById("minutes");
+const SecondsEl = document.getElementById("seconds");
 
+const birthDay = "28 May 2021 9:00 PM";
 
-const birthDay = '28 May 2021 9:00 PM';
+function countDown() {
+  const birthDayDate = new Date(birthDay);
+  const currentDate = new Date();
+  const totalSeconds = Math.floor((birthDayDate - currentDate) / 1000);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+  const minutes = Math.floor((totalSeconds / 60) % 60);
+  const seconds = Math.floor(totalSeconds % 60);
 
-function countDown(){
-	const birthDayDate = new Date(birthDay);
-	const currentDate = new Date();
-	const totalSeconds =Math.floor( (birthDayDate - currentDate) / 1000);
-	const days = Math.floor(totalSeconds / 3600 / 24);
-	const hours = Math.floor((totalSeconds / 3600) %24 );
-	const minutes = Math.floor((totalSeconds / 60) % 60);
-	const seconds = Math.floor((totalSeconds) % 60);
-
-	DaysEl.innerHTML = format(days);
-	HoursEl.innerHTML = format(hours);
-	MinutesEl.innerHTML = format(minutes);
-	SecondsEl.innerHTML = format(seconds);
+  DaysEl.innerHTML = format(days);
+  HoursEl.innerHTML = format(hours);
+  MinutesEl.innerHTML = format(minutes);
+  SecondsEl.innerHTML = format(seconds);
 }
 
-
-function format(time){
-	if(time>=0){
-	return time < 10 ? `0${time}` : time;
-	}
+function format(time) {
+  if (time >= 0) {
+    return time < 10 ? `0${time}` : time;
+  }
 }
 
-countDown(); 
+countDown();
 
-setInterval(countDown,1000);
+setInterval(countDown, 1000);
 
-const contactAgents = document.querySelector('.contact-agent');
+const contactAgents = document.querySelector(".contact-agent");
 
-contactAgents.addEventListener('click', () => {
-// console.log('clicked!');
-location.href = "./contact-agent.html";
+contactAgents.addEventListener("click", () => {
+  // console.log('clicked!');
+  location.href = "./contact-agent.html";
 });
