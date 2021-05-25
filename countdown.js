2c799f8376a03f22c3a944a9f8937c4477612ccc
Naresh <naresh.khatri2345@gmail.com>
2021-05-25 23:50:12 +0530

    Add whatsapp group link

--- countdown.js
+++ countdown.js
@@ -1,3 +1,4 @@
+'use strict'
 import { gamesStatus } from './api.config.js'
 
 const DaysEl = document.getElementById("days");
@@ -9,37 +10,44 @@ const gameTimeEl = document.getElementById("game-time");
 
 var gameTime = ''
 axios.get(gamesStatus)
-  .then(res => {
-    gameTime = Date.parse(res.data.start_time)
-    setDateAndTime(gameTime)
-    
-    countDown()
-  })
-  
-  function setDateAndTime(time){
-  console.log('hour = ' + new Date(time).getHours())
-  console.log('minute = ' + new Date(time).getMinutes())
-  gameDateEl.innerHTML = `${new Date(time).getDate()}/${new Date(time).getMonth()+1}/${new Date(time).getFullYear()}`
-  gameDateEl.innerHTML = `${new Date(time).Hours()}:${new Date(time).getMinutes()}/${new Date(time).getFullYear()}`
+    .then(res => {
+        gameTime = Date.parse(res.data.start_time)
+        setDateAndTime(gameTime)
+
+        countDown()
+    })
+
+function setDateAndTime(time) {
+    if (gameDateEl)
+        gameDateEl.innerHTML = `${new Date(time).getDate()}/${new Date(time).getMonth() + 1}/${new Date(time).getFullYear()}`
+
+    let hoursRemaining = new Date(time).getHours()
+    let minsRemaining = new Date(time).getMinutes()
+    if (gameTimeEl) {
+        if (hoursRemaining <= 12)
+            gameTimeEl.innerHTML = `${format(hoursRemaining)}:${format(minsRemaining)} AM`
+        else
+            gameTimeEl.innerHTML = `${format(hoursRemaining - 12)}:${format(minsRemaining)} PM`
+    }
 
 }
 function countDown() {
-  const currentDate = new Date();
-  const totalSeconds = Math.floor((gameTime - currentDate) / 1000);
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor((totalSeconds / 3600) % 24);
-  const minutes = Math.floor((totalSeconds / 60) % 60);
-  const seconds = Math.floor(totalSeconds % 60);
+    const currentDate = new Date();
+    const totalSeconds = Math.floor((gameTime - currentDate) / 1000);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor((totalSeconds / 3600) % 24);
+    const minutes = Math.floor((totalSeconds / 60) % 60);
+    const seconds = Math.floor(totalSeconds % 60);
 
-  DaysEl.innerHTML = format(days);
-  HoursEl.innerHTML = format(hours);
-  MinutesEl.innerHTML = format(minutes);
-  SecondsEl.innerHTML = format(seconds);
+    DaysEl.innerHTML = format(days);
+    HoursEl.innerHTML = format(hours);
+    MinutesEl.innerHTML = format(minutes);
+    SecondsEl.innerHTML = format(seconds);
 }
 
 function format(time) {
-  if (time >= 0) {
-    return time < 10 ? `0${time}` : time;
-  }
+    if (time >= 0) {
+        return time < 10 ? `0${time}` : time;
+    }
 }
 setInterval(countDown, 1000);
