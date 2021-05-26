49370efd35a523c462c490bec3c487a39d01b7ed
Naresh <naresh.khatri2345@gmail.com>
2021-05-27 03:27:00 +0530

    Add all functionalities to agent page such as auth, booking, logout, etc

--- api.config.js
+++ api.config.js
@@ -1,8 +1,9 @@
-//const prefix = 'http://127.0.0.1:8000/'
-const prefix = 'https://housie.plasmatch.in/'
+const prefix = 'http://127.0.0.1:8000/'
+//const prefix = 'https://housie.plasmatch.in/'
 
 const gamesStatus = prefix + 'api/gamesGet/1/'
 const getAllTickets = prefix + 'api/tickets/'
 const getTicket = prefix + 'api/ticketsGet/'
+const getToken = prefix + 'api/getToken/'
 
-export { gamesStatus, getTicket, getAllTickets }
+export { gamesStatus, getTicket, getAllTickets, getToken }
